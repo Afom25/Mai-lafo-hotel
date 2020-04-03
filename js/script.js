ea3dce517da490d709f0af851bb7f10d753b2375
hidri <agthidri2400@gmail.com>
2020-04-03 16:57:20 +0300

    this commit will add the wow js

--- js/script.js
+++ js/script.js
@@ -42,7 +42,39 @@ $(document).ready(function(){
         dots:false 
     });
   });
+ 
+  
+  function validation(){
+      var name = document.getElementById("name").Value;
+      var subject = document.getElementById("subject").value;
+      var phone = document.getElementById("phone").value;
+      var message = document.getElementById("message").value;
+      var email = document.getElementById("email").value;
+      var errorm = document.getElementById("errorm");
+      var text;
+  }
 
+    if(name.length < 2){
+        text = "please enter correcr name"
+        errorm.innerHTML = text;
+        return false;
+    }
+    if (email.indexOf("@") == -1 || email.lenght<5){
+        text = "please enter valid email";
+        errorm.innerHTML = text;
+        return false;
+    }
+    alert ("form submitted success")
+    $ (function(){
+        new WOW().init();
+      });
+      $(window).on('load', function(){
+            $("#home-heading-1").addClass("animated fadeInDown");
+            $("#home-heading-2").addClass("animated fadeInLeft");
+            $("#home-text").addClass("animated zoomIn");
+            $("#arrow-down i").addClass("animated fadeInDown infinite");
+      });
+    
 
 
 
