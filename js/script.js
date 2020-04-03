721f8307e936afe26ebe01347ee58aee52a7e13c
hidri <agthidri2400@gmail.com>
2020-04-03 22:28:07 +0300

    this commit will add the google map style

--- js/script.js
+++ js/script.js
@@ -1,40 +1,12 @@
-/*google map*/ 
-$(window).on ('load', function(){
-
-    var adressString = "Adi Quala, Eritrea";
-    var myLat = {lat:14.634720, lng:38.834259};
-
-    var map = new google.maps.Map(document.getElementById('map'),{
-        zoom:11,
-        center : myLat
-
-    });
-    // Add marker
-    var marker = new google.maps.Marker({
-        positon : myLat,
-        map:map ,
-        title: "click to see exact address"
-    })
-    // add Info window
-    var infowindow = new google.maps.InfoWindow({
-        content: adressString
-      });
-      // show info when user clicks marker
-      marker.addListener('click', function() {
-        infowindow.open(map, marker);
-      });
+$(window).on('load',function(){
+    $('#status').fadeOut();
+    $('#preloader').delay(300).fadeOut('slow');
 });
 
-
-
-
-// testimonials 
-
-
 $(document).ready(function(){
-    $("#testimonial-slider").owlCarousel({
+    $(".owl-carousel").owlCarousel({
         items:1,
-        autoplay:false,
+        autoplay:true,
         smartSpeed:500,
         loop:true,
         autoplayHoverPause:true,
@@ -42,41 +14,83 @@ $(document).ready(function(){
         dots:false 
     });
   });
- 
-  
+
+  /* progress bar */
+
+  $(function(){
+      $(".progress-bar").each(function(){
+          $(this).animate({
+              width: $(this).attr("aria-valuenow") + "%"}
+              ,1000);
+      });
+  });
+
   function validation(){
-      var name = document.getElementById("name").Value;
+      var name = document.getElementById("name").value;
       var subject = document.getElementById("subject").value;
       var phone = document.getElementById("phone").value;
       var message = document.getElementById("message").value;
       var email = document.getElementById("email").value;
       var errorm = document.getElementById("errorm");
       var text;
+
+     errorm.style.padding = "10px";
+
+     if (name.length < 2){
+         text = "please Enter Valid Name";
+         errorm.innerHTML = text;
+         return false ;
+     }
+    
+  if (email.indexOf("@") == -1 || email.length < 5){
+    text = "please Enter Valid email";
+    errorm.innerHTML = text;
+    return false ;
+
+}
+alert("form sunmitted success")
   }
 
-    if(name.length < 2){
-        text = "please enter correcr name"
-        errorm.innerHTML = text;
-        return false;
-    }
-    if (email.indexOf("@") == -1 || email.lenght<5){
-        text = "please enter valid email";
-        errorm.innerHTML = text;
-        return false;
-    }
-    alert ("form submitted success")
-    $ (function(){
-        new WOW().init();
-      });
-      $(window).on('load', function(){
-            $("#home-heading-1").addClass("animated fadeInDown");
-            $("#home-heading-2").addClass("animated fadeInLeft");
-            $("#home-text").addClass("animated zoomIn");
-            $("#arrow-down i").addClass("animated fadeInDown infinite");
-      });
+  /* animation*/
+
+  $ (function(){
+    new WOW().init();
+  });
+  $(window).on('load', function(){
+        $("#home-heading-1").addClass("animated pulse");
+        $("#home-head").addClass("animated fadeInLeft");
+        $("#home-text").addClass("animated zoomIn");
+        $("#arrow-down i").addClass("animated fadeInDown infinite");
+  });
+
     
+////** *** map */
 
+      
 
-
+        var adressString = "Adi Quala, Eritrea";
+        var myLat = {lat:14.634720, lng:38.834259};
+    
+        var map = new google.maps.Map(document.getElementById('map'),{
+            zoom:11,
+            center : myLat
+    
+        });
+        // Add marker
+        var marker = new google.maps.Marker({
+            positon : myLat,
+            map:map ,
+            title: "click to see exact address"
+        })
+        // add Info window
+        var infowindow = new google.maps.InfoWindow({
+            content: adressString
+          });
+          // show info when user clicks marker
+          marker.addListener('click', function() {
+            infowindow.open(map, marker);
+          });
+    
+    
 
 
